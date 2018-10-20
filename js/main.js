4a48e18ee52eefa63f4f9c82a4c6e8458790bbe0
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-10-20 13:35:47 -0700

    Add show functionality to hidden div content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 var link = document.getElementsByTagName('a');
 
+var bioDiv = document.getElementsByClassName('hide-div');
+
 function changeColor() {
 	this.classList.toggle('change-color');
+};
+
+function showDiv() {
+	this.nextElementSibling.classList.add('show-div');
 };
 
 for(i=0; i < link.length; i++) {
 	link[3].addEventListener('mouseenter', changeColor);
 	link[3].addEventListener('mouseleave', changeColor);
+	link[3].addEventListener('click', showDiv);
 };
 
 for(i=0; i < link.length; i++) {
