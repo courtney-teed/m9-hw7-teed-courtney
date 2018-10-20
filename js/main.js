31b160678c2ed4ed614379ddb61b5e509e5f03fe
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-10-20 13:12:09 -0700

    Add CSS hover styles to links

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var link = document.getElementsByTagName('a');
 
 function changeColor() {
-	
+	this.classList.toggle('change-color');
 };
 
 for(i=0; i < link.length; i++) {
-	link[i].addEventListener('mouseenter', changeColor);
-	link[i].addEventListener('mouseleave', changeColor);
+	link[3].addEventListener('mouseenter', changeColor);
+	link[3].addEventListener('mouseleave', changeColor);
+};
+
+for(i=0; i < link.length; i++) {
+	link[4].addEventListener('mouseenter', changeColor);
+	link[4].addEventListener('mouseleave', changeColor);
 };
