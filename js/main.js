97acd09594b97e1bb2dce71279a8e10a28406147
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-10-20 20:15:33 -0700

    Style CSS transition dropdown

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ function showDiv() {
 for(i=0; i < link.length; i++) {
 	link[3].addEventListener('mouseenter', changeColor);
 	link[3].addEventListener('mouseleave', changeColor);
-	link[3].addEventListener('click', showDiv);
 };
 
 for(i=0; i < link.length; i++) {
 	link[4].addEventListener('mouseenter', changeColor);
 	link[4].addEventListener('mouseleave', changeColor);
+	link[4].addEventListener('click', showDiv);
 };
